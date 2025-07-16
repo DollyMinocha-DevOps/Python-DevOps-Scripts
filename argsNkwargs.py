4ba4c61cc3547b03540e8792e4a31cf79165ad23
Dolly Minocha <ds834@yahoo.com>
2025-07-16 12:07:32 +0530

    Update argsNkwargs.py

--- argsNkwargs.py
+++ argsNkwargs.py
@@ -1,5 +1,6 @@
 # *args (variable arguments) & **kwargs (keyword arguments)
-
+# *args: Pass variable positional arguments as tuple
+# **kwargs: Pass variable keyword arguments as dict
 # used when we are not sure about the number of arguments to be passed to a function.
 
 def log(*argv, **kwargv):
